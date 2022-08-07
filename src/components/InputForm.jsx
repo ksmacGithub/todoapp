02ee64a60d6b45eada0de56606005952fd395f43
ksmacGithub <ks_129@icloud.com>
2022-08-07 23:36:44 +0900

    useState taskList, inputText追加

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const InputForm = () => {
+export const InputForm = ({taskList, setTaskList}) => {
+
+  const [inputText, setInputText] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setInputText(e.target.value);
+    console.log(inputText);
+  }
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={ handleSubmit }>
+        <input type="text" onChange={ handleChange } />
         <button>
-          button
+          <i className="fa-solid fa-square-plus"></i>
         </button>
       </form>
     </div>
